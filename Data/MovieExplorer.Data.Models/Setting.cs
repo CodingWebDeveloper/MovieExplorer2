9b00fe9c5bd9855484bb240d0108a24caec3a304
Miroslav Uzunov <miroslav.uzunov23@gmail.com>
2021-02-10 16:18:02 +0200

    Added Template

---
+++ Data/MovieExplorer.Data.Models/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieExplorer.Data.Models
+{
+    using MovieExplorer.Data.Common.Models;
+
+    public class Setting : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
