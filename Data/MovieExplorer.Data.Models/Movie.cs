b41b257ab82b875774302d77711758c85d9c4115
MiroslavUzunov <72207223+CodingWebDeveloper@users.noreply.github.com>
2021-02-12 14:23:19 +0200

    Revert "Configuations for my tables"

--- Data/MovieExplorer.Data.Models/Movie.cs
+++ Data/MovieExplorer.Data.Models/Movie.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -15,25 +17,21 @@ namespace MovieExplorer.Data.Models
 
         public string Title { get; set; }
 
-        public DateTime? ReleaseDate { get; set; }
+        public DateTime ReleaseDate { get; set; }
 
         public int Minutes { get; set; }
 
-        public double? Rate { get; set; }
+        public double Rate { get; set; }
 
         public int ImageId { get; set; }
 
-        public virtual MovieImage Image { get; set; }
-
         public string Description { get; set; }
+
+        public virtual MovieImage MovieImage { get; set; }
 
         public virtual Director Director { get; set; }
 
         public int DirectorId { get; set; }
-
-        public int CountryId { get; set; }
-
-        public Country Country { get; set; }
 
         public virtual ICollection<MovieGenre> Genres { get; set; }
     }
--- Data/MovieExplorer.Data.Models/MovieImage.cs
+++ Data/MovieExplorer.Data.Models/MovieImage.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace MovieExplorer.Data.Models
 {
@@ -10,12 +9,6 @@ namespace MovieExplorer.Data.Models
         public int Id { get; set; }
 
         public string Url { get; set; }
-
-        public string Extensions { get; set; }
-
-        public int MovieId { get; set; }
-
-        public Movie Movie { get; set; }
 
     }
 }
